c71b9d4ea6e948372e7e212bd3fb20870d9e8e5d
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-08-28 13:45:04 +0200

    statistics functionnal

--- js/statistics.js
+++ js/statistics.js
@@ -7,23 +7,35 @@ httpRequest.onreadystatechange = function() {
         if (httpRequest.status === 200) {
             // la réponse est exploitable et valide
             // on affiche la réponse sur la page
-            let user = JSON.parse(httpRequest.responseText);
             // put the function
-
-        } 
+            let stats = JSON.parse(httpRequest.responseText);
+            const tbody = document.getElementById("list");
+            const thead = document.getElementById("headList");
+            const tr = document.createElement("tr");
+                for (const property in stats) {
+                    const th = document.createElement("th");
+                    const td = document.createElement("td");
+                    th.innerText = property;
+                    td.innerText = stats[property];
+                    tr.appendChild(td);
+                    thead.appendChild(th);
+                    tbody.appendChild(tr);
+                }
+                
+            } 
         else {
             // il y a eu un problème avec la requête,
             // par exemple la réponse peut être un code 404 (Non trouvée) 
             // ou 500 (Erreur interne au serveur)
             console.log("une erreur est survenue");
         }
-    } 
+    }
     else {
         // pas encore prête
         console.log("en attente de réponse");
     }
 };
 // ouverture et envoi de la requête
-httpRequest.open('GET', 'user.Json', true);
+httpRequest.open('GET', 'statistics.json', true);
 httpRequest.send();
-}
+
