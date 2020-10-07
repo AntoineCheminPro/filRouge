3d4e9e24a6e2bb4b60ad84bf2b433d11b7bfd815
Antoine Chemin <antoine.chemin.pro@outlook.fr>
2020-10-07 16:46:53 +0200

    add DB to directory

--- banque_php.sql
+++ banque_php.sql
@@ -1,66 +1,167 @@
-DROP DATABASE IF EXISTS banque_php;
-CREATE DATABASE banque_php CHARACTER SET 'utf8';
-USE banque_php;
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 07 oct. 2020 à 16:45
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.4.9
 
-DROP USER IF EXISTS 'BanquePHP'@'Localhost';
-CREATE USER 'BanquePHP'@'Localhost';
-GRANT ALL PRIVILEGES ON banque_php.* To 'BanquePHP'@'Localhost' IDENTIFIED BY 'banque76';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE User(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  lastname VARCHAR(50) NOT NULL,
-  firstname VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL UNIQUE,
-  city VARCHAR(30) NOT NULL,
-  city_code CHAR(5) NOT NULL,
-  adress VARCHAR(50) NOT NULL,
-  sex CHAR(1) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  birth_date DATE NOT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO User(lastname, firstname, email, city, city_code, adress, sex, password, birth_date)
-VALUES
-("Dupont", "Richard", "r.dupont@gmail.com", "Rouen", "76000", "9 rue du gros horloge", "h", "Riri1962!", "1962-05-21"),
-("Melez", "Claire", "clairemelez@outlook.com", "Lille", "59100", "45 rue du Molinel", "f", "AstraGirl154", "1989-11-14");
+--
+-- Base de données : `banque_php`
+--
 
-CREATE TABLE Account(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  amount DECIMAL(11,2) NOT NULL,
-  opening_date TIMESTAMP NOT NULL,
-  account_type VARCHAR(50) NOT NULL,
-  user_id INT UNSIGNED,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES User(id)
-)
-ENGINE=InnoDB;
+-- --------------------------------------------------------
 
-INSERT INTO Account(amount, opening_date, account_type, user_id)
-VALUES
-(596.23, NOW(), "Compte courant", 1),
-(12345, NOW(), "Livret A", 1),
-(500, NOW(), "PEL", 1),
-(-56.78, NOW(), "Compte courant", 2);
+--
+-- Structure de la table `account`
+--
 
-CREATE TABLE Operation(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  operation_type VARCHAR(30) NOT NULL,
-  amount DECIMAL(11,2) NOT NULL,
-  registered TIMESTAMP NOT NULL,
-  label VARCHAR(50) NULL,
-  account_id INT UNSIGNED,
-  PRIMARY KEY (id),
-  FOREIGN KEY (account_id) REFERENCES Account(id)
-)
-ENGINE=InnoDB;
+CREATE TABLE `account` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `amount` decimal(11,2) NOT NULL,
+  `opening_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `account_type` varchar(50) NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO Operation(operation_type, amount, registered, label, account_id)
-VALUES
-("débit", -15.60, NOW(), "le poulet d'or SARL", 1),
-("crédit", 500, NOW(), NULL, 2),
-("débit", -7.62, NOW(), "carrefour essence", 1),
-("débit", -50, NOW(), "frais de gestion", 2)
-;
+--
+-- Déchargement des données de la table `account`
+--
+
+INSERT INTO `account` (`id`, `amount`, `opening_date`, `account_type`, `user_id`) VALUES
+(1, '596.23', '2020-10-05 09:15:12', 'Compte courant', 1),
+(2, '12345.00', '2020-10-05 09:15:12', 'Livret A', 1),
+(3, '500.00', '2020-10-05 09:15:12', 'PEL', 1),
+(4, '-56.78', '2020-10-05 09:15:12', 'Compte courant', 2),
+(5, '200.00', '2020-10-05 12:42:09', 'livret A', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `operation`
+--
+
+CREATE TABLE `operation` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `operation_type` varchar(30) NOT NULL,
+  `amount` decimal(11,2) NOT NULL,
+  `registered` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `label` varchar(50) DEFAULT NULL,
+  `account_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `operation`
+--
+
+INSERT INTO `operation` (`id`, `operation_type`, `amount`, `registered`, `label`, `account_id`) VALUES
+(1, 'débit', '-15.60', '2020-10-05 09:15:12', 'le poulet d\'or SARL', 1),
+(2, 'crédit', '500.00', '2020-10-05 09:15:12', NULL, 2),
+(3, 'débit', '-7.62', '2020-10-05 09:15:12', 'carrefour essence', 1),
+(4, 'débit', '-50.00', '2020-10-05 09:15:12', 'frais de gestion', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `city_code` char(5) NOT NULL,
+  `adress` varchar(50) NOT NULL,
+  `sex` char(1) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birth_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `lastname`, `firstname`, `email`, `city`, `city_code`, `adress`, `sex`, `password`, `birth_date`) VALUES
+(1, 'Dupont', 'Richard', 'r.dupont@gmail.com', 'Rouen', '76000', '9 rue du gros horloge', 'h', '$2y$10$UHuchDFaKZsXttavIk13pue2FqClHp7zUtbHx5jSAF52CswmAPB6G', '1962-05-21'),
+(2, 'Melez', 'Claire', 'clairemelez@outlook.com', 'Lille', '59100', '45 rue du Molinel', 'f', 'AstraGirl154', '1989-11-14');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `operation`
+--
+ALTER TABLE `operation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `operation`
+--
+ALTER TABLE `operation`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `operation`
+--
+ALTER TABLE `operation`
+  ADD CONSTRAINT `operation_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
